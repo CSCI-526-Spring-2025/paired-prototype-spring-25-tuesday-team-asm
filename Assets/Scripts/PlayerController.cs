99550a996fa78875ecd9a754ddf9975cd25c1038
Mohamed <mohalibou@icloud.com>
2025-02-03 14:18:53 -0800

    Adding more floors as well as collision.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float mouseSensitivity = 100f;  // Increased sensitivity
+    public float mouseSensitivity = 100f;
 
     private float xRotation = 0f;
     private Transform playerCamera;
+    private Rigidbody rb;
 
     void Start()
     {
         playerCamera = GetComponentInChildren<Camera>().transform;
+        rb = GetComponent<Rigidbody>();
 
+        // Lock the cursor to the game window
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        // --- Player Movement ---
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
-
         // --- Camera & Player Rotation ---
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -34,5 +30,15 @@ public class PlayerController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        // --- Player Movement ---
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
     }
 }
